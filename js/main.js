fcad084a1a1df10d7e04894665459f4ed549a6d3
선규 <106080105+sungyu1@users.noreply.github.com>
2023-01-18 22:00:55 +0900

    js: 버튼기능추가

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
-const moreBtn=document.querySelector('.info .metadata .moreBtn');
-const title =document.querySelector('.info .metadata .title');
+const moreBtn=document.querySelector('.moreBtn');
+const title =document.querySelector('.title');
+// 좋아요 버튼
+const resultElement = document.getElementById('result');
+let number = resultElement.innerText;
 
-// moreBtn.addEventListener('click',()=>{
-//   moreBtn.classList.toggle('clicked');
-//   title.classList.toggle('clamp');
-// });
+
+
+
+
+
+moreBtn.addEventListener('click',()=>{
+  moreBtn.classList.toggle('clicked');
+  title.classList.toggle('clamp');
+});
+
+function count(type)  {
+  // 결과를 표시할 element
+  
+  // 현재 화면에 표시된 값
+  
+  // 더하기/빼기
+  if(type === 'plus') {
+    number = parseInt(number) + 1;
+  }else if(type === 'minus')  {
+    number = parseInt(number) - 1;
+  }
+  // 결과 출력
+  resultElement.innerText = number;
+}
